88974ee83a6782dae3f6f6571ef1d2b10c98471e
skywolf46 <skywolf461@gmail.com>
2022-06-29 14:45:04 +0900

    - Add test class for SignalUtilTest

---
+++ src/test/kotlin/skywolf46/extrautility/core/test/data/TestSignalData.kt
@@ -0,0 +1,6 @@
+package skywolf46.extrautility.core.test.data
+
+class TestSignalData {
+    var isTestCompleted = false
+    var isPriorityTestCompleted = false
+}
---
+++ src/test/kotlin/skywolf46/extrautility/core/test/data/TestSignalObject.kt
@@ -0,0 +1,40 @@
+package skywolf46.extrautility.core.test.data
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import skywolf46.extrautility.core.annotations.SignalReceiver
+import skywolf46.extrautility.core.test.exceptions.TestFailedException
+
+object TestSignalObject {
+    @SignalReceiver
+    private fun TestSignalData.onSignal() {
+        isTestCompleted = true
+    }
+
+    @SignalReceiver(priority = 20)
+    private fun TestSignalData.onPrioritySignal() {
+        assertFalse(isPriorityTestCompleted)
+    }
+
+    @SignalReceiver(priority = 40)
+    private fun TestSignalData.onPrioritySignal2() {
+        isPriorityTestCompleted = true
+    }
+
+    @SignalReceiver(priority = 80)
+    private fun TestSignalData.onPrioritySignal3() {
+        assertTrue(isPriorityTestCompleted)
+    }
+
+    @SignalReceiver
+    private fun TestSignalData.onSignalButWillFailed(data: Any) {
+        throw TestFailedException()
+    }
+
+
+    @SignalReceiver
+    private fun onSignalButWillFailed() {
+        throw TestFailedException()
+    }
+}
