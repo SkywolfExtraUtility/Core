1f64b5b48fcfd8ad288ee9370737ca1686b7abbc
skywolf46 <skywolf461@gmail.com>
2022-06-29 19:19:02 +0900

    - Add RejectAutoRegister to reject register and use manual registering

---
+++ src/main/kotlin/skywolf46/extrautility/core/annotations/RejectAutoRegister.kt
@@ -0,0 +1,5 @@
+package skywolf46.extrautility.core.annotations
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RejectAutoRegister
