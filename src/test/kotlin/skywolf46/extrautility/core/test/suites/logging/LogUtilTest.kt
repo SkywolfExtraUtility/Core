507abbbda7a46471f502fbed4fb69a5d76e95996
skywolf46 <skywolf461@gmail.com>
2022-06-29 14:44:24 +0900

    - Add test for LogUtil

--- src/test/kotlin/skywolf46/extrautility/core/test/suites/logging/LogUtilTest.kt
+++ src/test/kotlin/skywolf46/extrautility/core/test/suites/logging/LogUtilTest.kt
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
+import skywolf46.extrautility.core.test.data.TestSignalData
 import skywolf46.extrautility.core.util.*
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class LogUtilTest {
-    private val prefix = "ExtraUtility - TestLogging | "
+    private val prefix = "ExtraUtility - Core/Test | "
+    private val prefixClass = "ExtraUtility - Core/Log Test | "
+    private val prefixSpecifiedClass = "ExtraUtility - Core/Log Test 2 | "
 
     @Order(0)
     @Test
@@ -43,15 +46,28 @@ class LogUtilTest {
         assertEquals(prefix, appendPrefix(""))
     }
 
+    @Test
+    @Order(4)
+    fun testCurrentClassPrefix() {
+        bindPrefix(prefixClass)
+        assertEquals(prefixClass, findPrefix())
+    }
 
     @Test
-    @Order(3)
+    @Order(5)
+    fun testSpecifiedClassPrefix() {
+        bindPrefixTo(TestSignalData::class, prefixSpecifiedClass)
+        assertEquals(prefixSpecifiedClass, findPrefixFor(TestSignalData::class))
+    }
+
+    @Test
+    @Order(6)
     fun testLogging() {
         log("Test logging")
     }
 
     @Test
-    @Order(5)
+    @Order(7)
     fun testLoggingError() {
         logError("Test error message")
     }
