70acf9c62751e593331160d7eaa6fade5b7b2a7b
skywolf46 <skywolf461@gmail.com>
2022-06-29 23:22:20 +0900

    fix: change MutableMap construction to associateWith

--- src/test/kotlin/skywolf46/extrautility/core/test/suites/reflection/ReflectionUtilTest.kt
+++ src/test/kotlin/skywolf46/extrautility/core/test/suites/reflection/ReflectionUtilTest.kt
@@ -1,9 +1,20 @@
 package skywolf46.extrautility.core.test.suites.reflection
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
 import org.junit.jupiter.api.Test
-import skywolf46.extrautility.core.util.ReflectionUtil
+import org.junit.jupiter.api.TestMethodOrder
+import skywolf46.extrautility.core.test.abstraction.CoreRequiredTest
 
 
-class ReflectionUtilTest {
+@TestMethodOrder(OrderAnnotation::class)
+class ReflectionUtilTest : CoreRequiredTest() {
 
+    @Test
+    fun test() {
+
+    }
+
+    fun is한글() {
+
+    }
 }
